47b8222669da5ad86717b32c32b443abd0355b23
Tu Nombre <you@example.com>
2024-05-29 11:54:18 +0200

    aplicados estilos globales

--- header.php
+++ header.php
@@ -12,30 +12,32 @@
 
 <body>
     <header class="site-header">
-        <div class="logo">
-            <a href="<?=esc_url(home_url('/'))?>">
-                <img class="logo-image" src="<?= get_template_directory_uri() ?>/images/logo.png" alt="logo">
-            </a>
-        </div><!--.logo-->
-        <div class="header-information">
-            <div class="socials">
-                <?php
-                   $args=array(
-                    'theme_location'=> 'social-menu',
-                    'container'=> 'nav',
-                    'container_class'=> 'socials',
-                    'container_id'=> 'socials',
-                    'link_before' => '<span class="sr-text">',
-                    'link_after'=> '</span>',
-                   );
-                   wp_nav_menu($args);
-                ?>
-            </div><!--.socials-->
-            <div class="address">
-                <p>07023 Calle Moscas, 1A</p>
-                <p>Telófono: 666555444</p>
+        <div class="container">
+            <div class="logo">
+                <a href="<?=esc_url(home_url('/'))?>">
+                    <img class="logo-image" src="<?= get_template_directory_uri() ?>/images/logo.png" alt="logo">
+                </a>
+            </div><!--.logo-->
+            <div class="header-information">
+                <div class="socials">
+                    <?php
+                       $args=array(
+                        'theme_location'=> 'social-menu',
+                        'container'=> 'nav',
+                        'container_class'=> 'socials',
+                        'container_id'=> 'socials',
+                        'link_before' => '<span class="sr-text">',
+                        'link_after'=> '</span>',
+                       );
+                       wp_nav_menu($args);
+                    ?>
+                </div><!--.socials-->
+                <div class="address">
+                    <p>07023 Calle Moscas, 1A</p>
+                    <p>Telófono: 666555444</p>
+                </div>
             </div><!--Header Information-->
-        </div>
+        </div><!--container-->
     </header>
     <div class="main-menu">
         <div class="navigation">
